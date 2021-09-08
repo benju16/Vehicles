fb8cf6bbc7252e2b41687b04a903583d2734a803
YEISON DAVID BENJUMEA GUTIERREZ <yeisonbenjumea187359@correo.itm.edu.co>
2021-09-07 20:19:24 -0500

    Improve vehicle type controller

--- Vehicles.API/Data/Entities/VehicleType.cs
+++ Vehicles.API/Data/Entities/VehicleType.cs
@@ -9,7 +9,7 @@ namespace Vehicles.API.Data.Entities
     public class VehicleType
     {
         public int Id { get; set; }
-        [Display(Name = "Tipo de vehiculo")]
+        [Display(Name = "Tipo de vehículo")]
         [MaxLength(50, ErrorMessage = "El campo {0}  no puede tener más de {1} carácteres")]
         [Required(ErrorMessage = "ElCampo {0} es obligatorio.")]
         public string  Description { get; set; }
